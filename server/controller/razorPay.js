dafc035a542b2e239d257e5a772af6f3c31ccc45
anuragV1089 <anuragv1248@gmail.com>
2025-07-12 00:16:26 +0530

    Razorpay initiation

---
+++ server/controller/razorPay.js
@@ -0,0 +1,12 @@
+import Razorpay from "razorpay";
+import Order from "../models/order";
+
+const razorpay = new Razorpay({
+  key_id: process.env.RAZORPAY_KEY_ID,
+  key_secret: process.env.RAZORPAY_KEY_SECRET,
+});
+
+export const createRazorpayOrder = async (req, res) => {
+  try {
+  } catch (err) {}
+};
---
+++ server/models/order.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const orderSchema = Schema({
+  razorpay_order_id: {
+    type: String,
+    required: true,
+  },
+  razorpay_payment_id: String,
+  razorpay_signature: String,
+  amount: Number,
+  status: String,
+  paymentVerified: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const Order = mongoose.model("Order", orderSchema);
+module.exports = Order;
